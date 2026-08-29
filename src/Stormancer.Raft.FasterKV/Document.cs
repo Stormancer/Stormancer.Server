e1a12e29c15126f77bf909d0ef26b65351df4826
Jean-Michel Deruty <jmderuty@stormancer.com>
2026-08-29 15:40:13 +0200

    integrate FasterKV as a storage backend option.

---
+++ src/Stormancer.Raft.FasterKV/Document.cs
@@ -0,0 +1,35 @@
+﻿using FASTER.core;
+using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stormancer.Raft.FasterKV
+{
+
+    [MessagePackObject(true)]
+    public class Document
+    {
+        public int Version { get; internal set; }
+      
+        public required object Content { get; set; }
+
+    }
+    internal class EntrySerializer : BinaryObjectSerializer<Document>
+    {
+        public override void Deserialize(out Document obj)
+        {
+
+            obj = MessagePack.MessagePackSerializer.Deserialize<Document>(reader.BaseStream);
+        }
+
+        public override void Serialize(ref Document obj)
+        {
+
+            MessagePack.MessagePackSerializer.Serialize(writer.BaseStream, obj);
+            
+        }
+    }
+}
---
+++ src/Stormancer.Raft.FasterKV/FasterKVDatabase.cs
@@ -0,0 +1,19 @@
+﻿using FASTER.core;
+using Stormancer.Raft.WAL;
+using System;
+
+namespace Stormancer.Raft.FasterKV
+{
+    public class FasterKVDatabase : IWalDatabase
+    {
+        private readonly FasterKV<string, Document> _store;
+        public FasterKVDatabase(FasterKV<string, Document> store)
+        {
+            _store = store;
+        }
+        public bool TryApplyRecord(IRecord record)
+        {
+            _store.For().NewSession();
+        }
+    }
+}
